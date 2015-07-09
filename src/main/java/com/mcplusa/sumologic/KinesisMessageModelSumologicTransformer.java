eac7d5d8e44ef4bd556f2e5d9a51c654f867780d
Juan Pablo Diaz-Vaz <jpdiazvaz@Juan-Pablos-MacBook-Pro.local>
2015-07-09 16:31:24 -0300

    SUMOK-16 Configuration for CloudWatch logs

--- src/main/java/com/mcplusa/sumologic/KinesisMessageModelSumologicTransformer.java
+++ src/main/java/com/mcplusa/sumologic/KinesisMessageModelSumologicTransformer.java
@@ -1,29 +1,70 @@
 package com.mcplusa.sumologic;
 
-import com.amazonaws.services.kinesis.connectors.BasicJsonTransformer;
+import java.io.IOException;
 
+import com.amazonaws.services.kinesis.connectors.BasicJsonTransformer;
+import com.amazonaws.services.kinesis.model.Record;
 import com.mcplusa.sumologic.KinesisMessageModel;
+import com.mcplusa.sumologic.implementations.SumologicEmitter;
 import com.mcplusa.sumologic.implementations.SumologicTransformer;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.io.ByteArrayInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.zip.GZIPInputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+import org.apache.commons.codec.binary.Base64;
 
 
 /**
  * A custom transfomer for {@link KinesisMessageModel} records in JSON format. The output is in a format
  * usable for insertions to Sumologic.
  */
-public class KinesisMessageModelSumologicTransformer extends
-        BasicJsonTransformer<KinesisMessageModel, String> implements
+public class KinesisMessageModelSumologicTransformer implements
         SumologicTransformer<KinesisMessageModel> {
 
+  private static final Log LOG = LogFactory.getLog(KinesisMessageModelSumologicTransformer.class);
+  
     /**
      * Creates a new KinesisMessageModelSumologicTransformer.
      */
     public KinesisMessageModelSumologicTransformer() {
-        super(KinesisMessageModel.class);
+        super();
     }
 
     @Override
     public String fromClass(KinesisMessageModel message) {
         return message.toString();
+    }
+
+    @Override
+    public KinesisMessageModel toClass(Record record) throws IOException {
+      byte[] decodedRecord = record.getData().array();
+      String stringifiedRecord = decompressGzip(decodedRecord);
+
+      return new KinesisMessageModel(stringifiedRecord);
+    }
+    
+    public static String decompressGzip(byte[] compressedData) {
+      try {
+        GZIPInputStream gis = new GZIPInputStream(new ByteArrayInputStream(compressedData));
+        BufferedReader bf = new BufferedReader(new InputStreamReader(gis, "UTF-8"));
+        
+        String outStr = "";
+        String line;
+        while ((line=bf.readLine())!=null) {
+          outStr += line;
+        }
+        return outStr;
+      } catch (IOException exc) {
+        LOG.warn("Exception during decompression of data: " + exc.getMessage());
+        return null;
+      }
     }
 
 }
